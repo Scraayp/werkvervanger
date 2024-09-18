f9b3d68d6c51576c10f1f66f04d203b2d0d06d1e
Michal <hello@michalk.nl>
2024-09-18 14:17:49 +0200

    (feat) Added breeze template for auth

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
+});
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Authentication
-Route::get('/login', function () {
-    return view('login');
-})->name('auth.login');
-
-Route::get('/register', function () {
-    return view('register');
-})->name('auth.register');
-
-Route::get('/forget-password', function () {
-    return view('forget-password');
-})->name('auth.forget-password');
-
+require __DIR__.'/auth.php';
